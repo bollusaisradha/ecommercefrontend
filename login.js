f118a3a7664564c6c50c7bc4aecd1603630fd522
= <=>
2025-08-19 21:07:58 +0530

    ecommerce page created

--- login.js
+++
@@ -1,6 +0,0 @@
-const login=()=>{
-    return`
-    <h1>iam login page</h1>
-    `
-}
-export default login
--- register.js
+++
@@ -1,6 +0,0 @@
-const register=()=>{
-    return`
-    <h1>iam register page</h1>
-    `
-}
-export default register
